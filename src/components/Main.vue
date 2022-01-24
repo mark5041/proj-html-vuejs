b3f2aa423f4c0c6902552e77f279184bcda93e98
mark5041 <mark.beloso5041@gmail.com>
2022-01-24 15:06:36 +0100

    add icons

---
+++ src/components/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+// import axios from 'axios'
+// import Velocity from 'velocity-animate'
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+
+
+<style lang="scss">
+@import "./assets/scss/style.scss";
+@import "~bootstrap/scss/bootstrap";
+</style>
