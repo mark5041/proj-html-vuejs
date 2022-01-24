2e62019aad55df6a9a0ff841d6d2f7dc41176296
mark5041 <mark.beloso5041@gmail.com>
2022-01-24 10:58:27 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
   </div>
 </template>
 
+// import axios from 'axios'
+// import Velocity from 'velocity-animate'
+
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 
@@ -16,13 +19,9 @@ export default {
 };
 </script>
 
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/scss/style.scss";
+@import "~bootstrap/scss/bootstrap";
 </style>
